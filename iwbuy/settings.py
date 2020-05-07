8111d3274b759233587d2e108e5a78ff009136f6
dimmoon69 <pldimitry@yandex.ru>
2020-05-07 11:47:04 +0300

    cors

--- iwbuy/settings.py
+++ iwbuy/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
 
     'channels',
 
+    'corsheaders',
+
     # 'allauth',
     # 'allauth.account',
 
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -100,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # django-allauth
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
